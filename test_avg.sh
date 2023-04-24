b8307213309dff702a369982478971110836acb3
yzzzz <92359632+Fyuzn@users.noreply.github.com>
2023-04-24 14:53:09 +0800

    Update test_avg.sh

--- test_avg.sh
+++ test_avg.sh
@@ -1,7 +1,7 @@
 modelfile=$1  
 dataset=$2
 num_checkpoints=$3
-python3 LNMT-CA/scripts/average_checkpoints.py --inputs checkpoints/$modelfile --num-epoch-checkpoints $num_checkpoints --output checkpoints/$modelfile/average_epoch.pt 
+python3 scripts/average_checkpoints.py --inputs checkpoints/$modelfile --num-epoch-checkpoints $num_checkpoints --output checkpoints/$modelfile/average_epoch.pt 
 
 fairseq-generate data-bin/$dataset --gen-subset test --path checkpoints/$modelfile/average_epoch.pt --beam 5 --remove-bpe --lenpen 0.6 --no-progress-bar --log-format json > results/pred_${modelfile}_test_avg_last_${num_checkpoints}epoch
 fairseq-generate data-bin/$dataset --gen-subset test1 --path checkpoints/$modelfile/average_epoch.pt --beam 5 --remove-bpe --lenpen 0.6 --no-progress-bar --log-format json > results/pred_${modelfile}_test1_avg_last_${num_checkpoints}epoch
